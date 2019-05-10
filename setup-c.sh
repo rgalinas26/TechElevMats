4a035a931bac0a585f9bd99162afb5f20ad79537
Mike Morel <michael.morel@techelevator.com>
2019-05-10 09:27:16 -0400

    Instructor push

---
+++ setup-c.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Change this to match your repository root
+readonly TEAM_REPO="https://bitbucket.org/te-cle-cohort-11"
+readonly cohort="c"
+
+echo
+read -r -p "Enter your name (First Last)? " name
+read -r -p "Enter your email? " email
+
+reponame=${name//[[:blank:]]/}
+
+
+echo
+echo "Setting Up Global Configuration Settings"
+
+git config --global user.name "${name}"
+git config --global user.email "${email}"
+
+echo "Setting up Git Editors and Tools..."
+
+git config --global core.editor "code -w -n"
+git config --global diff.tool code
+git config --global difftool.code.cmd "code -w -d \$LOCAL \$REMOTE"
+
+echo
+echo "Cloning Repositories..."
+
+git clone "${TEAM_REPO}/${reponame}-${cohort}"
+
+echo
+echo "Configuring Upstream..."
+
+cd "${reponame}-${cohort}"
+git remote add upstream "${TEAM_REPO}/${cohort}-main"
+git config branch.master.mergeOptions "--no-edit"
+
+echo "Done."
