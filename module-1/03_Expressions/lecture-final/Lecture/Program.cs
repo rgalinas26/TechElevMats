8a04af9089241393d3c94fbac86fe9c920a1a207
Mike Morel <mike@techelevator.com>
2019-05-15 21:34:41 -0400

    Instructor push for M01.04

---
+++ module-1/03_Expressions/lecture-final/Lecture/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LectureExample example = new LectureExample();
+
+            int number = example.ReturnNotOne();
+            Console.WriteLine(number);
+
+            double doubleNumber = example.ReturnNotHalf();
+            Console.WriteLine(doubleNumber);
+
+            string name = example.ReturnName();
+            Console.WriteLine(name);
+
+            double singleValue = 10;
+            double product = example.ReturnDoubleOf(singleValue);
+            Console.WriteLine(product);
+
+            int age = 25;
+            string category = example.ReturnAdultOrMinor(age);
+            Console.WriteLine("Age " + age + " is a " + category);
+
+            age = 17;
+            category = example.ReturnAdultOrMinor(age);
+            Console.WriteLine("Age " + age + " is a " + category);
+
+            age = 18;
+            category = example.ReturnAdultOrMinor(age);
+            Console.WriteLine("Age " + age + " is a " + category);
+
+            Console.ReadKey();
+        }
+    }
+}
