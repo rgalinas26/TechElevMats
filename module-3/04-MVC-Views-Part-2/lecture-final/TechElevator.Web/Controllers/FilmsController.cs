826e227c3e00d9e9fd13e6b062f97065ad5212bb
Mike Morel <mike@techelevator.com>
2019-06-27 13:34:43 -0400

    M3D4 lecture code

---
+++ module-3/04-MVC-Views-Part-2/lecture-final/TechElevator.Web/Controllers/FilmsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TechElevator.Web.DAL;
+using TechElevator.Web.Models;
+
+namespace TechElevator.Web.Controllers
+{
+    public class FilmsController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Get a list of all films (our Model)
+            IStarWarsDAO dao = new MockStarWarsDAO();
+            IList<Film> films = dao.GetFilms();
+
+            return View(films);
+        }
+
+        public IActionResult Detail(string id)
+        {
+            IStarWarsDAO dao = new MockStarWarsDAO();
+            Film film = dao.GetFilm(id);
+
+            if (film == null)
+            {
+                return NotFound();
+
+            }
+            else
+            {
+                return View(film);
+            }
+        }
+    }
+}
