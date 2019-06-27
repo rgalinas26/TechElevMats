d64b8671f8fde7fb20e67aec6fed58a4fabaf821
Mike Morel <mike@techelevator.com>
2019-06-27 08:27:35 -0400

    M3D3 lecture and M3D4 student

---
+++ module-3/03-Introduction-to-MVC-Views/lecture-final/TechElevator.Web/Controllers/CalculatorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechElevator.Web.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index(int? maxKPH)
+        {
+            if (maxKPH.HasValue)
+            {
+                ViewData["max"] = maxKPH.Value;
+            }
+            else
+            {
+                ViewData["max"] = 200;
+            }
+            return View();
+        }
+    }
+}
