bd6cfb7f89774352fdb6267463597995ff5dfaad
Mike Morel <mike@techelevator.com>
2019-06-30 17:31:39 -0400

    M3D6 student

---
+++ module-3/06-Forms-and-Controllers-HTTP-GET/student-exercise/dotnet/GETForms.Web/DAL/IFilmDAO.cs
@@ -0,0 +1,17 @@
+﻿using GETForms.Web.Models;
+using System.Collections.Generic;
+
+namespace GETForms.Web.DAL
+{
+    public interface IFilmDAO
+    {
+        /// <summary>
+        /// Searches for films within the given parameters.
+        /// </summary>
+        /// <param name="genre"></param>
+        /// <param name="minLength"></param>
+        /// <param name="maxLength"></param>
+        /// <returns></returns>
+        IList<Film> GetFilmsBetween(string genre, int minLength, int maxLength);
+    }
+}
