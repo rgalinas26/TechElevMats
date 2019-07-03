f30148cb5c081211571d0402f4f74c4a679be404
Mike Morel <mike@techelevator.com>
2019-07-02 22:26:43 -0400

    M3D8 student

---
+++ module-3/08-Session-and-Flash-Scope/student-lecture/SessionCart/SessionCart.Web/Controllers/StoreController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SessionCart.Web.DAL;
+using SessionCart.Web.Models;
+using Newtonsoft.Json;
+
+namespace SessionCart.Web.Controllers
+{
+    public class StoreController : Controller
+    {
+        /* Steps
+           1. (StoreController)    Create Index Action for store/index
+           2. (Index View)         Create Index View for store/index
+           TEST
+           3. (Startup.cs)         Configure DI container to inject IProductDAL > FakeProductDAL
+           4. (StoreController)    Enable constructor injection for IProductDAL
+           5. (StoreController)    Update Index Action to pass products
+           6. (Index View)         Display Products
+           TEST
+           7. (Index View)         Add Form Tag w/ AddToCart button > POST to store/index
+                                   Pass Product Id in Form
+           8. (StoreController)    Create POST Index Action for store/index, accept product id & 
+                                   redirect to store/viewcart            
+           9. (StoreController)    Create ViewCart action for store/viewcart
+           10.(ViewCart View)      Display empty View Cart View
+           TEST
+           11.(StoreController)    POST store/index: retrieve product and add to active
+                                   shopping cart
+           12.(ShoppingCart)       Create Shopping Cart class to add Product & Quantity
+           13.(StoreController)    Guarantee ShoppingCart is in session, create if not
+           14.(StoreController)    POST store/index: Update shopping cart with new product
+           15.(StoreController)    GET store/viewcart: Retrieve shoping cart from session
+           16.(ViewCart View)      Bind go ShoppingCart and disply each product in it
+       */
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
