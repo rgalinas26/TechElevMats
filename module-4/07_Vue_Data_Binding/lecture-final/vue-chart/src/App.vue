091f9b9e241b3ca6f1cbe39e7a60b2d4332d91a5
Mike Morel <mike@techelevator.com>
2019-07-24 14:49:58 -0400

    M4D7 lecture and M4D8 tutorial

---
+++ module-4/07_Vue_Data_Binding/lecture-final/vue-chart/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <nutrition-chart class="nutrition"></nutrition-chart>
+  </div>
+</template>
+
+<script>
+import Nutrition  from './components/Nutrition.vue'
+
+export default {
+  name: 'app',
+  data() { return {color: 'Red'}},
+  components: {
+    'nutrition-chart' : Nutrition
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 100%;
+}
+</style>
