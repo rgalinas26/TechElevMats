0c153bc6ed310f6e096c116d21b8e24b7012e296
Mike Morel <mike@techelevator.com>
2019-07-26 12:52:12 -0400

    M4D9 lecture and M4D10 tutorial

---
+++ module-4/09_Vue_Nested_Components/lecture-final/vue-chart/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <nutrition-chart class="nutrition" v-on:dataChanged="handleDataChange"></nutrition-chart>
+    <br/>
+    <h3>The Chart</h3>
+    <stacked-bar-chart v-bind:chartData="nutritionChartData"></stacked-bar-chart>
+  </div>
+</template>
+
+<script>
+import Nutrition  from './components/Nutrition.vue'
+import StackedBarChart  from './components/StackedBarChart.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'nutrition-chart' : Nutrition,
+    'stacked-bar-chart' : StackedBarChart
+  },
+  data() {
+    return {
+      nutritionChartData : []
+    }
+  },
+  methods: {
+    handleDataChange(eventChartData) {
+        this.nutritionChartData = eventChartData;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 100%;
+}
+</style>
---
+++ module-4/09_Vue_Nested_Components/lecture-final/vue-chart/src/components/StackedBarChart.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="barchart">
+      <div class="barchart-bar" v-for="(item, index) in chartData" v-bind:key="index"  v-bind:style="{'width': item.percent, 'background-color' : item.color}">{{item.percent}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        chartData : Array
+    },
+    data() {
+        return {
+        }
+    }
+};
+
+</script>
+
+<style scoped>
+.barchart {
+  height: 40px;
+  width: 80%;
+  display: flex;
+  flex-direction: row;
+  border: 1px solid darkgray;
+  box-shadow: 5px 5px lightgray;
+}
+
+.barchart-bar {
+  height: 100%;
+  text-align: center;
+  border-top: 1px solid darkgray;
+  border-right: 1px solid darkgray;
+}
+</style>
