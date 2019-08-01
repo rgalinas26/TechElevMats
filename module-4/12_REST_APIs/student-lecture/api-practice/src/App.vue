bebf8f47930f44c09db785ce5d56043df792879b
Mike Morel <mike@techelevator.com>
2019-08-01 08:56:41 -0400

    M4D12 student

---
+++ module-4/12_REST_APIs/student-lecture/api-practice/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <to-do />
+  </div>
+</template>
+
+<script>
+import Todo from '@/components/Todo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'to-do' : Todo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
